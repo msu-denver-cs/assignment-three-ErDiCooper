c6d75c204435f7a8e919fd98e58e3a5e8be828d4
ErDiCooper <ericdicarlo@hotmail.com>
2019-12-07 17:22:04 -0700

    Updates to fix Issue #s 5, 6, and 7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :cars
-  resources :makes
-  resources :parts
+  resources :cars do
+	collection do
+		get 'search'
+		end
+	end
+  resources :makes do
+  collection do
+		get 'search'
+		end
+	end
+  resources :parts do
+  collection do
+		get 'search'
+		end
+	end
   
   root to: "cars#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
